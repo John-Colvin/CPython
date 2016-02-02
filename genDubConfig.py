d8cd1bc0b5cefff0453a5cb2cc612b279400b53d
John Colvin <john.loughran.colvin@gmail.com>
2016-02-02 13:35:15 +0000

    add libs to genDubConfig.py

--- genDubConfig.py
+++ genDubConfig.py
@@ -10,7 +10,10 @@ if len(sys.argv) < 2:
 
 name = sys.argv[1]
 vinfo = sys.version_info
+py2 = vinfo[0] == 2
+py3 = vinfo[0] == 3
 libdir = sysconfig.get_config_var("LIBDIR")
+libname = "python" + sysconfig.get_config_var('VERSION') + (sys.abiflags if py3 else '')
 
 print(
 '''{
@@ -22,6 +25,7 @@ print(
     "dependencies": { "cpython": { "path": "../../" } },
     "subConfigurations": { "cpython": "python''' + str(vinfo[0]) + str(vinfo[1]) + '''" },
     "lflags-posix": ["-L''' + libdir + '''"],
+    "libs": ["''' + libname + '''"],
     "postBuildCommands-posix": [
         "mv lib$$DUB_TARGET_NAME.so $$DUB_TARGET_NAME.so"
     ]
